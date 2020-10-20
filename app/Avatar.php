47f0b8fa83adc47f502808f456db9840f7bee1ae
Alex <gurievalex78@gmail.com>
2020-10-20 13:08:42 +0200

    fillable

--- app/Avatar.php
+++ app/Avatar.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Avatar extends Model
 {
     public $timestamps = false;
+    
     protected $fillable = [
         'telefono', 'avatar', 'user_id'
     ];
