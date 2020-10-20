faea7473ab0d5c8c163558e921b920ee5ed86575
Alex <gurievalex78@gmail.com>
2020-10-20 13:12:44 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+    <li><a href="{{route('users.index')}}">Visualizza Utenti</a></li>
+        <li><a href="">Visualizza Post</a></li>
+        <li>Crea Post</li>
+    </ul>
+</nav>
